ed6485be1488324b3979d8a5e213291b90276c82
Dan Lepadatu <lepadatudan@gmail.com>
2016-09-29 18:40:54 +0200

    refactor: remove debugg data from AddSceneController

--- src/ApplicationLayer/AddSceneController.java
+++ src/ApplicationLayer/AddSceneController.java
@@ -9,15 +9,15 @@ import Kino.KinoXP;
  */
 public class AddSceneController {
 
-    /*textFieldName.getText(), Integer.parseInt(textFieldDuration.getText()), Double.parseDouble(textFieldPrice.getText()),
-            textFieldActors.getText(), textFieldDescription.getText(), coverPath.getText(),textFieldAgeLimit.getText(),textFieldGenre.getText(),textFieldRating.getText())*/
-    public void saveData(String name, int duration, double price, String actors, String description, String cover, String age, String genre, String rating) {
-        Movie movie = new Movie(name, duration, price, actors, description, cover, age, genre, rating);
+    public void saveData(String name, int duration, double price, String actors, String description,
+                         String cover, String age, String genre, String rating) {
+        // create a new movie
+        Movie movie = new Movie(name, duration, price, actors,
+                description, cover, age, genre, rating);
 
+        // add the new movie to the global ObservableList and DB
         KinoXP.movieList.add(movie);
         DBMovies dbMovies = new DBMovies();
         dbMovies.insert(movie);
-
-
     }
 }

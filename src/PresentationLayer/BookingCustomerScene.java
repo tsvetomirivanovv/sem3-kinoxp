bf415f44dcaf3363eca93e4505fe08aa64a024a2
Tsvetomir Ivanov <tsvetomirivanovv@gmail.com>
2016-10-04 12:32:15 +0200

    Labels textfields

--- src/PresentationLayer/BookingCustomerScene.java
+++ src/PresentationLayer/BookingCustomerScene.java
@@ -1,7 +1,33 @@
 package PresentationLayer;
 
-/**
- * Created by Tsvetomir on 10/4/2016.
- */
-public class BookingCustomerScene {
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BookingCustomerScene extends Application {
+    private VBox vlabels, vtextfilds;
+    private Label namelabel, phoneNo, email;
+    private TextField nameTextField, phoneTextField, emailTextField,  ticketTextField;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+         namelabel = new Label("Name of the client");
+         phoneNo = new Label("Phone number");
+         email = new Label("Email");
+
+         nameTextField = new TextField();
+        nameTextField.setPromptText("Write name here...");
+
+        phoneTextField = new TextField();
+        phoneTextField.setPromptText("Write phone number here...");
+
+        emailTextField = new TextField();
+        emailTextField.setPromptText("Write email here...");
+
+        
+
+    }
 }

238458c83044e1baf086cd0971e6ab9142efcc6e
Dana Iliescu <dana.iliescu96@gmail.com>
2016-10-04 13:54:19 +0200

    for searching a movie in the system/database

--- src/ApplicationLayer/ManageMovieController.java
+++ src/ApplicationLayer/ManageMovieController.java
@@ -5,6 +5,10 @@ import DataAccessLayer.DBMovies;
 import Kino.KinoXP;
 import PresentationLayer.AddButtonScene;
 import PresentationLayer.ViewMovieScene;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * Created by Tsvetomir on 9/28/2016.
@@ -34,5 +38,34 @@ public class ManageMovieController {
     public void showInfoScene(Movie movie) {
         ViewMovieScene viewMovieScene = new ViewMovieScene();
         viewMovieScene.setInfoScene(movie);
+    }
+
+    public void searchMovie(TableView moviesTableView, TextField searchField){
+        FilteredList<Movie> filteredData = new FilteredList<>(moviesTableView.getItems(), p -> true);
+
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(movie -> {
+                // If filter text is empty, display all movies.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // Compare title of every movie with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (movie.getName().toLowerCase().contains(lowerCaseFilter))
+                    return true; // Filter matches title.
+                return false; // Does not match.
+            });
+        });
+
+        // Wrap the FilteredList in a SortedList.
+        SortedList<Movie> sortedData = new SortedList<>(filteredData);
+
+        // Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(moviesTableView.comparatorProperty());
+
+        // Add sorted (and filtered) data to the table.
+        moviesTableView.setItems(sortedData);
     }
 }
